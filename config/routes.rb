fd3a4650a7f912ecf749bad468503e3decd2a960
Ubuntu <ubuntu@ip-172-31-24-127.us-east-2.compute.internal>
2019-07-07 13:16:15 +0000

    crud done

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'homes/index'
   get 'homes/new'
   get 'homes/create'
-  get 'homes/show'
-  get 'homes/edit'
-  get 'homes/update'
-  get 'homes/destroy'
+  get 'homes/show/:id' => 'homes#show'
+  get 'homes/edit/:id' => 'homes#edit'
+  get 'homes/update/:id' => 'homes#update'
+  get 'homes/destroy/:id' => 'homes#destroy'
+  
+  root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
